eea62bc75596f62a8006453291e14dc14c19fb23
Pearl <pearl.rathour33@gmail.com>
2023-07-13 14:21:59 +0530

    Added Links

--- src/components/HowitWorks.jsx
+++ src/components/HowitWorks.jsx
@@ -1,66 +1,70 @@
 import React from 'react';
 
 import { PhoneIcon, ArrowSmRightIcon, MapIcon, ShieldCheckIcon, ClipboardCopyIcon, SearchIcon } from '@heroicons/react/outline';
-import {ChipIcon, ClipboardCheckIcon, SupportIcon} from '@heroicons/react/solid'
+import { ChipIcon, ClipboardCheckIcon, SupportIcon } from '@heroicons/react/solid'
 
 import supportImg from '../assets/Parking.jpg'
 
 const HowitWorks = () => {
   return (
-  <div name='howitworks' className='w-full mt-24'>
+    <div name='howitworks' className='w-full mt-24'>
       <div className='w-full h-[700px] bg-gray-900/90 absolute'>
         <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
       </div>
-      
+
       <div className='max-w-[1240px] mx-auto text-white relative'>
-          <div className='px-4 py-12'>
-              {/* <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>How it Works ? </h2> */}
-              <h3 className='text-5xl font-bold py-6 text-center'>How it Works ?</h3>
-              <p className='py-4 text-3xl text-slate-300'>ParkShare is a unique solution to the parking problem that provides a platform for connecting parking space owners with those in need of parking.</p>
+
+        <div className='px-4 py-12'>
+          {/* <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>How it Works ? </h2> */}
+          <h3 className='text-5xl font-bold py-6 text-center'>How it Works ?</h3>
+          <p className='py-4 text-3xl text-slate-300'>ParkShare is a unique solution to the parking problem that provides a platform for connecting parking space owners with those in need of parking.</p>
+        </div>
+
+        <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+
+          <div className='bg-white rounded-xl shadow-2xl'>
+            <div className='p-8'>
+              <ClipboardCopyIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+              <h3 className='font-bold text-2xl my-6'>How to list parking space ?</h3>
+              <p className='text-gray-600 text-xl'>List your unused parking space on ParkShare platform, providing information on its location, availability, and pricing and let others park their vehicles on it.</p>
+            </div>
+            <div className='bg-slate-100 pl-8 py-4'>
+              <a href="https://parkshare11.onrender.com/register">
+                <p className='flex items-center text-indigo-600'>Register <ArrowSmRightIcon className='w-5 ml-2' /></p>
+              </a>
+            </div> 
           </div>
 
-          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
-              <div className='bg-white rounded-xl shadow-2xl'>
-                  <div className='p-8'>
-                      <ClipboardCopyIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
-                      <h3 className='font-bold text-2xl my-8'>How to list parking space ?</h3>
-                      <p className='text-gray-600 text-xl'>List your unused parking space on ParkShare platform, providing information on its location, availability, and pricing and let others park their vehicles on it.</p>
-                  </div>
-                  <div className='bg-slate-100 pl-8 py-4'>
-                    <a href="https://parkshare11.onrender.com/register">
-                      <p className='flex items-center text-indigo-600'>Register <ArrowSmRightIcon className='w-5 ml-2' /></p>
-                    </a>
-                  </div>
-              </div>
-              <div className='bg-white rounded-xl shadow-2xl'>
-                  <div className='p-8'>
-                      <SearchIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
-                      <h3 className='font-bold text-2xl my-6'>How to find a parking spot?</h3>
-                      <p className='text-gray-600 text-xl'>Browse through the available parking spots in the desired location, view pricing and availability and book the one that meets your needs online through the ParkShare platform.
-</p>
-                  </div>
-                  <div className='bg-slate-100 pl-8 py-4'>
-                    <a href="https://parkshare11.onrender.com/toilink">
-                      <p className='flex items-center text-indigo-600'>Search <ArrowSmRightIcon className='w-5 ml-2' /></p>
-                    </a>
-                  </div>
-              </div>
-              <div className='bg-white rounded-xl shadow-2xl'>
-                  <div className='p-8'>
-                      <ShieldCheckIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
-                      <h3 className='font-bold text-2xl my-6'>Security Handling</h3>
-                      <p className='text-gray-600 text-xl'>User-friendly and secure platform with a rating system for reliable parking experiences.
-</p>
-                  </div>
-                  <div className='bg-slate-100 pl-8 py-4'>
-                  <a href="mailto:21bec055@nith.ac.in">
-                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
-                    </a>
-                  </div>
-              </div>
+          <div className='bg-white rounded-xl shadow-2xl'>
+            <div className='p-8'>
+              <SearchIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+              <h3 className='font-bold text-2xl my-6'>How to find a parking spot?</h3>
+              <p className='text-gray-600 text-xl'>Explore parking spots in the desired location, view pricing and availability and book the one that meets your needs online through the ParkShare platform.
+              </p>
+            </div>
+            <div className='bg-slate-100 pl-8 py-4'>
+              <a href="https://parkshare11.onrender.com/toilink">
+                <p className='flex items-center text-indigo-600'>Search <ArrowSmRightIcon className='w-5 ml-2' /></p>
+              </a>
+            </div>
           </div>
+
+          <div className='bg-white rounded-xl shadow-2xl'>
+            <div className='p-8'>
+              <ShieldCheckIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+              <h3 className='font-bold text-2xl my-6'>Security Handling</h3>
+              <p className='text-gray-600 text-xl'>User-friendly and secure platform with a rating system for reliable parking experiences.
+              </p>
+            </div>
+            <div className='bg-slate-100 pl-8 py-4 my-10'>
+              <a href="mailto:pearl.rathour33@gmail.com">
+                <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, animateScroll as scroll, } from 'react-scroll'
 
 
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
 
-    const handleClose =()=> setNav(!nav)
+  const handleClose = () => setNav(!nav)
 
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
@@ -16,35 +16,42 @@ const Navbar = () => {
         <div className='flex items-center'>
           <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>ParkShare</h1>
           <ul className='hidden md:flex'>
-          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-          <li><Link to="howitworks" smooth={true} offset={-50} duration={500}>How it Works</Link></li>
-          <li><Link to="benefits" smooth={true} offset={-100} duration={500}>Benefits</Link></li>
-          <li><Link to="Joinnow" smooth={true} offset={-50} duration={500}>Join Now</Link></li>
+            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+            <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+            <li><Link to="howitworks" smooth={true} offset={-50} duration={500}>How it Works</Link></li>
+            <li><Link to="benefits" smooth={true} offset={-100} duration={500}>Benefits</Link></li>
+            <li><Link to="Joinnow" smooth={true} offset={-50} duration={500}>Join Now</Link></li>
           </ul>
         </div>
+
         <div className='hidden md:flex pr-4'>
-          <button className='border-none bg-transparent text-black mr-4'>
-            Sign In
-          </button>
-          <button className='px-8 py-3'>Sign Up</button>
+          <a href="https://parkshare11.onrender.com/register">
+            <button className='px-8 py-3 mx-2 bg-transparent border-solid text-indigo-600 hover:bg-indigo-600 hover:text-white'>
+              Sign In
+            </button>
+          </a>
+
+          <a href="https://parkshare11.onrender.com/login">
+            <button className='px-8 py-3 mx-2'>Sign Up</button>
+          </a>
         </div>
+        
         <div className='md:hidden mr-4' onClick={handleClick}>
-            {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
-          
+          {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
+
         </div>
       </div>
 
       <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="howitworks" smooth={true} offset={-50} duration={500}>Support</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="benefits" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
-          <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="joinnow" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="howitworks" smooth={true} offset={-50} duration={500}>Support</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="benefits" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="joinnow" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
 
         <div className='flex flex-col my-4'>
-            <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
-            <button className='px-8 py-3'>Sign Up</button>
+          <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+          <button className='px-8 py-3'>Sign Up</button>
         </div>
       </ul>
     </div>
